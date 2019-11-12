fcf7d5b5410da7bb97cab20ed52016c6b5181eba
serena Lee <dearserenalee@gmail.com>
2019-11-12 08:51:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end 
+
+def assign_rooms(speakers)
+  array[]
+  speakers.each do |speaker|
+  array << ("Hello #{name}! You'll be assigned to #{room}")
+end
+return array
+end  
